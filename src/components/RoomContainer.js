969d68ce530d28158df84f4913559dc023c12ec9
Kerron S. Streater <kerronstreater@gmail.com>
2019-12-15 16:02:44 -0500

    fourth commit

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import RoomFilter from './RoomFilter';
 import RoomList from './RoomList';
-;import { RoomConsumer } from '../context';
+import { withRoomConsumer } from '../context';
+import Loading from './Loading';
+
+function RoomContainer({context}) {
+    const { loading, sortedRooms, rooms } = context;
+    if(loading){
+        return <Loading />
+    }
+    return (
+        <div>
+            hello from rooms container
+            <RoomFilter rooms={rooms} />
+            <RoomList rooms={sortedRooms} />
+        </div>
+    );
+
+}
+
+export default withRoomConsumer(RoomContainer);
+
+/*
+import React from 'react';
+import RoomFilter from './RoomFilter';
+import RoomList from './RoomList';
+import { RoomConsumer } from '../context';
 import Loading from './Loading';
 
 export default function RoomContainer() {
@@ -26,3 +50,4 @@ export default function RoomContainer() {
 
     )
 }
+*/
